878a42299fa3af4e3e5593f6426aa266526bb433
Thiago da Silva Nascimento <thiag.g3@gmail.com>
2018-08-13 15:30:31 -0300

    Fixing unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { TemplateModule } from '@app/template/template.module';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        TemplateModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -18,10 +22,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('awesome-tastes-discovery');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should have one children', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to awesome-tastes-discovery!');
+    expect(compiled.querySelector('app-warp-container')).toBeTruthy();
   }));
 });
