4d9c9e91a40fd393a58d5381bb133d01764897f9
Thiago da Silva Nascimento <thiag.g3@gmail.com>
2018-08-15 14:19:52 -0300

    Adding first-letter-upper.pipe

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageComponent } from './page/page.component';
+import { FirstLetterUpperPipe } from './first-letter-upper.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [PageComponent],
-  exports: [PageComponent]
+  declarations: [PageComponent, FirstLetterUpperPipe],
+  exports: [PageComponent, FirstLetterUpperPipe]
 })
 export class FeatureModule { }
