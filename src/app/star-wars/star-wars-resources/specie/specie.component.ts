69bb086c5f2dd88fe2fd0d5918c6638c7123127e
Thiago Nascimento <thiag.g3@gmail.com>
2018-08-17 17:43:24 -0300

    Improviments for star-wars.component and specie.component

--- src/app/star-wars/star-wars-resources/specie/specie.component.ts
+++ src/app/star-wars/star-wars-resources/specie/specie.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-specie',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./specie.component.scss']
 })
 export class SpecieComponent implements OnInit {
+
+  @Input()
+  specie: any = {};
 
   constructor() { }
 
