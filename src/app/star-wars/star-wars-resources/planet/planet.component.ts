119c056d245e13e15c68b5727b6f1ab9e200086a
Thiago Nascimento <thiag.g3@gmail.com>
2018-08-17 17:46:06 -0300

    Improviments for star-wars.component and planet.component

--- src/app/star-wars/star-wars-resources/planet/planet.component.ts
+++ src/app/star-wars/star-wars-resources/planet/planet.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-planet',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planet.component.scss']
 })
 export class PlanetComponent implements OnInit {
+
+  @Input()
+  planet: any = {};
 
   constructor() { }
 
