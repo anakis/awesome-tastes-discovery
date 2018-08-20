30e878495e2667d2a504dc9fd0269d0c06f829de
Thiago da Silva Nascimento <thiag.g3@gmail.com>
2018-08-20 17:49:26 -0300

    Creating components of marvel resources

--- src/app/marvel/marvel-resources/marvel-resources.module.ts
+++ src/app/marvel/marvel-resources/marvel-resources.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CharacterComponent } from './character/character.component';
+import { ComicComponent } from './comic/comic.component';
+import { CreatorComponent } from './creator/creator.component';
+import { EventComponent } from './event/event.component';
+import { SerieComponent } from './serie/serie.component';
+import { StorieComponent } from './storie/storie.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [CharacterComponent, ComicComponent, CreatorComponent, EventComponent, SerieComponent, StorieComponent]
 })
 export class MarvelResourcesModule { }
