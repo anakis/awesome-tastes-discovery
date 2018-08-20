fb45d9dd0ffcbfe22f4f35d089530c2b78a5d16a
Thiago da Silva Nascimento <thiag.g3@gmail.com>
2018-08-20 11:29:38 -0300

    Removing 'protected' from data and type on star-wars-resources.component

--- src/app/star-wars/star-wars-resources/star-wars-resources.component.ts
+++ src/app/star-wars/star-wars-resources/star-wars-resources.component.ts
@@ -7,12 +7,12 @@ export abstract class StarWarsResourcesComponent implements OnInit {
    * data object to be displayed
    */
   @Input()
-  protected data: any = {};
+  data: any = {};
 
   /**
    * type of resource to be handle in LikeList screen
    */
-  protected readonly type: String = 'star-wars';
+  readonly type: String = 'star-wars';
 
   constructor() { }
 
