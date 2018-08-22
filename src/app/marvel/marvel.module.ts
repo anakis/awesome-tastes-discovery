cf552afde2d5255a5d2642ebf6e9ab13bafb5722
Thiago da Silva Nascimento <thiag.g3@gmail.com>
2018-08-22 17:32:30 -0300

    fet: Adding styles to marvel screen

--- src/app/marvel/marvel.module.ts
+++ src/app/marvel/marvel.module.ts
@@ -5,13 +5,15 @@ import { MarvelRoutingModule } from './marvel-routing.module';
 import { MarvelComponent } from './marvel.component';
 import { FeatureModule } from '@app/feature/feature.module';
 import { MarvelResourcesModule } from '@app/marvel/marvel-resources/marvel-resources.module';
+import { TemplateModule } from '@app/template/template.module';
 
 @NgModule({
   imports: [
     CommonModule,
     MarvelRoutingModule,
     FeatureModule,
-    MarvelResourcesModule
+    MarvelResourcesModule,
+    TemplateModule
   ],
   declarations: [MarvelComponent]
 })
