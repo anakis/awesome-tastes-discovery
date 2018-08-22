886a9dfa93697ed0ac039ea45f3cbcb35d7e5ed7
Thiago da Silva Nascimento <thiag.g3@gmail.com>
2018-08-22 16:41:09 -0300

    fet: Adding new templates components to export array

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { WarpComponent } from './warp/warp.component';
 import { WarpContainerComponent } from './warp-container/warp-container.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { CardComponent } from './card/card.component';
+import { RowComponent } from './row/row.component';
+import { ColComponent } from './col/col.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [WarpComponent, WarpContainerComponent, WelcomeComponent],
-  exports: [WarpComponent, WarpContainerComponent, WelcomeComponent]
+  declarations: [WarpComponent, WarpContainerComponent, WelcomeComponent, CardComponent, RowComponent, ColComponent],
+  exports: [WarpComponent, WarpContainerComponent, WelcomeComponent, CardComponent, RowComponent, ColComponent]
 })
 export class TemplateModule { }
